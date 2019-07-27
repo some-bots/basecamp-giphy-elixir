d109ba677f87ffec2fd2ad838591ccc8b988b43b
Michael Minton <michaelcminton@gmail.com>
2019-07-27 10:48:04 -0500

    Bump to Elixir 1.9

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule BasecampGiphyElixir.MixProject do
     [
       app: :basecamp_giphy_elixir,
       version: "0.1.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
